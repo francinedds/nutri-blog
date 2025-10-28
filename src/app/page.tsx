e9b8ead5ce6182dd2b9002eab4871f6064ad9a1b
Francine Danielle <137731537+francinedds@users.noreply.github.com>
2025-10-28 15:15:42 +0000

    fix: remove margin y

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
         </div>
       </section>
 
-      <hr className="border-t border-[#9DAB91] w-[900px] my-4" />
+      <hr className="border-t border-[#9DAB91] w-[900px]" />
 
       <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-6xl mt-6">
         {posts.map((post, index) => (
