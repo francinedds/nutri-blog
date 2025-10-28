d3140f9a62a06039b8ecb1e34b870ddd33b696dc
Francine Danielle <137731537+francinedds@users.noreply.github.com>
2025-10-28 12:31:17 +0000

    fix: remove justify-center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center grow bg-[#F8F7F2] px-6 py-12 space-y-16 w-full">
+    <main className="flex flex-col items-center grow bg-[#F8F7F2] px-6 py-12 space-y-16 w-full">
       <section className="text-center max-w-2xl">
         <h1 className="text-5xl font-amatic font-bold text-[#9DAB91] mb-2">Seu progresso começa no prato!</h1>
         <p className="text-[#C69168] text-sm font-poppins mb-6">Receitas, mitos e verdades: tudo o que você precisa para comer bem.</p>
