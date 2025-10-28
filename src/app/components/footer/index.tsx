dfeb115e082103b8aedad48a586a4726c9e3b23d
francine <francined71@gmail.com>
2025-10-27 22:32:08 -0300

    feat: add footer

---
+++ src/app/components/footer/index.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="w-full bg-[#9DAB91] py-6">
+      <div className="flex items-center justify-between px-6">
+        <Link href="/" className="flex items-center space-x-2">
+          <Image
+            src="/logo.svg"
+            alt="NutriViva logo"
+            width={100}
+            height={100}
+            priority
+          />
+        </Link>
+
+        <div className="flex flex-wrap justify-center gap-32">
+          <Link href="/terms" className="text-sm text-white hover:underline">
+            Termos de Uso
+          </Link>
+          <Link href="/privacy" className="text-sm text-white hover:underline">
+            Política de Privacidade
+          </Link>
+          <span className="text-sm text-white">Direitos Reservados</span>
+        </div>
+      </div>
+    </footer>
+  );
+}
