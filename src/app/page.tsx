cb1739a8b48afe03affbe54a04c04a2406ffe5e8
Francine Danielle <137731537+francinedds@users.noreply.github.com>
2025-10-28 13:00:10 +0000

    fix: format code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center grow bg-[#F8F7F2] px-6 py-6 space-y-10 w-full">
       <div className="flex justify-end w-full">
-          <SearchInput />
+        <SearchInput />
       </div>
       <section className="text-center max-w-2xl">
         <h1 className="text-5xl font-amatic font-bold text-[#9DAB91] mb-2">
