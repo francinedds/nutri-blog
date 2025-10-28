e4ab51e5d82fbcfe3ad4f706f6ffcc56e877ba58
Francine Danielle <137731537+francinedds@users.noreply.github.com>
2025-10-28 12:59:04 +0000

    feat: add search input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import SearchInput from "./components/search-input";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center grow bg-[#F8F7F2] px-6 py-12 space-y-16 w-full">
+    <main className="flex flex-col items-center grow bg-[#F8F7F2] px-6 py-6 space-y-10 w-full">
+      <div className="flex justify-end w-full">
+          <SearchInput />
+      </div>
       <section className="text-center max-w-2xl">
-        <SearchInput />
         <h1 className="text-5xl font-amatic font-bold text-[#9DAB91] mb-2">
           Seu progresso começa no prato!
         </h1>
