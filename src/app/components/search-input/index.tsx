2c3c92dfc9192babf0af7be7d60ec41e30b65f0f
Francine Danielle <137731537+francinedds@users.noreply.github.com>
2025-10-28 12:49:18 +0000

    fix: input size

--- src/app/components/search-input/index.tsx
+++ src/app/components/search-input/index.tsx
@@ -12,7 +12,7 @@ export default function SearchInput({
   onChange,
 }: SearchInputProps) {
   return (
-    <div className="flex items-center border border-[#9DAB91] rounded-none px-3 py-2 w-full max-w-sm">
+    <div className="flex items-center border border-[#9DAB91] rounded-none px-3 py-2 w-[300px] max-w-sm">
       <Search className="text-[#9DAB91] w-5 h-5 mr-2" />
       <input
         type="text"
