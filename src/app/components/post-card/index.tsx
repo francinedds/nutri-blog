73a017ac7f5c889a7d36f45c370e768dcff9c7ee
francine <francined71@gmail.com>
2025-11-04 13:43:05 -0300

    fix: add fixed size to post cards

--- src/app/components/post-card/index.tsx
+++ src/app/components/post-card/index.tsx
@@ -17,10 +17,10 @@ export default function PostCard({
   link = "#",
 }: PostCardProps) {
   return (
-    <div>
+    <div className="h-[380px]">
       <Link
         href={link}
-        className="block border border-[#9DAB91] rounded-md overflow-hidden shadow-sm hover:shadow-md transition-shadow bg-[#F8F7F2]"
+        className="block border border-[#9DAB91] rounded-md overflow-hidden shadow-sm hover:shadow-md transition-shadow bg-[#F8F7F2] h-full"
       >
         {imageUrl && (
           <div className="relative w-full h-40">
@@ -33,11 +33,11 @@ export default function PostCard({
             />
           </div>
         )}
-        <div className="p-4 flex flex-col space-y-2">
-          <h2 className="text-sm font-semibold text-gray-800">
-            {title}
-          </h2>
-          <p className="text-sm text-gray-600 text-justify">{description}</p>
+        <div className="p-4 flex flex-col space-y-2 h-[calc(100%-10rem)]">
+          <h2 className="text-sm font-semibold text-gray-800">{title}</h2>
+          <p className="text-sm text-gray-600 text-justify grow overflow-hidden text-ellipsis">
+            {description}
+          </p>
           {date && <span className="flex justify-end text-xs">{date}</span>}
         </div>
       </Link>
